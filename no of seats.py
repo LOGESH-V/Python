a997a2b8b6833fb75b11b33358a8a0629aec966b
LOGESH-V <113714374+LOGESH-V@users.noreply.github.com>
2024-03-17 12:32:00 +0530

    Add files via upload

---
+++ no of seats.py
@@ -0,0 +1,6 @@
+#program to solve a given problem
+
+people_watching=15768
+empty_seats=34890
+Total_seats=people_watching+empty_seats
+print("The total number of seats in the stadium is",Total_seats)
